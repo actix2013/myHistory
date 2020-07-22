aeae12f3cb9d8374217d067ab9539b3e2e7038b3
Guillaume CAVALIE <actix2013@gmail.com>
2020-07-22 16:57:34 +0200

    create skills and categories

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
+
 
 class AppFixtures extends Fixture
 {
@@ -12,6 +14,75 @@ class AppFixtures extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
+        $values = [
+            "SOFT SKILLS" =>
+            [
+                "Motivé",
+                "Persévérant",
+                "Capacité d’adaptation",
+                "Réaliste",
+                "Sérieux",
+            ],
+            "CENTRE D'INTERETS" =>
+            [
+                "Informatique",
+                "Lou rugby",
+                "Lecture SF / Fantasy",
+            ],
+            "LANGUES" =>
+            [
+                "Francais",
+                "Anglais Technique",
+            ],
+
+            "Languages" =>
+                [
+                    "HTML",
+                    "CSS",
+                    "JS",
+                    "Java"
+                ],
+            "Shell" =>
+                [
+                    "Batch",
+                    "Cmd",
+                    "Powershell"
+                ],
+            "Frameworks" =>
+                [
+                    "Symfony",
+                    "MVC",
+                    "Bootstrap",
+                    "Worflow Git",
+                ],
+
+            "Outils de collaboration " =>
+                [
+                    "Git/GitHub",
+                    "Methode Agile/Scrum",
+
+                ],
+
+        ];
+
+        $cat = 0;
+        $skillNb = 0;
+        foreach ($values as $key => $skills) {
+            $category = new Category();
+            $category->setName($key);
+            $manager->persist($category);
+            $this->addReference("category_" . $cat, $category);
+            for ($a = 0; $a < count($skills); $a++) {
+                    $skill = new Skill();
+                    $skill->setName($skills[$a]);
+                    $skill->setCategory($this->getReference("category_" . $cat));
+                    $this->addReference("skill_" . $cat . "_" . $a, $skill);
+                    $this->addReference("skillNb_$skillNb", $skill);
+                    $skillNb++;
+                    $manager->persist($skill);
+            }
+            $cat++;
+        }
         $manager->flush();
     }
 }
