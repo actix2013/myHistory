93cbfb959ebf36629cec4e4d5d17530237e3ac88
Guillaume CAVALIE <actix2013@gmail.com>
2020-07-23 23:29:01 +0200

    create controler admin implement embeded controlerr

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -4,7 +4,23 @@
 namespace App\Controller;
 
 
-class AdministrationController
+use App\Repository\TaskRepository;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdministrationController extends AbstractController
 {
 
+
+    /**
+     * @Route("/admin/skills", name="admin_skills", methods={"GET"})
+     */
+    public function adminSkills(): Response
+    {
+
+        return $this->render('admin/admin.html.twig',["show"=>"skills"]);
+
+    }
 }
