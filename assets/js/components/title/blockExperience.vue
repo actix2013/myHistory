c9e6e31c75e11b2110a0031cd1dd43b602550ee1
Guillaume CAVALIE <actix2013@gmail.com>
2020-12-05 19:41:59 +0100

    WIP reactive text titles axios in progress

--- assets/js/components/title/blockExperience.vue
+++ assets/js/components/title/blockExperience.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
-        <h2 class="text-danger">
+
+        <h2 class="text-danger"
+            @mouseover="hover = true"
+            @mouseleave="hover = false"
+        >
             <strong>
                 {{ title }}
             </strong>
@@ -9,16 +13,42 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     data: () => {
         return {
+            hover: false,
             title: 'EXPERIENCES'
         }
     },
    mounted: function () {
-        const dataTitle = this.$parent;
-        console.log('datatitle: ', this.$refs.my-title);
+
     },
+    methods: {
+        send() {
+
+            const data = {};
+            data['history'] = {"userName": "postman", "description": "created by postman", "source": "postman"};
+            data['top'] = {"userName": "top", "description": "top", "source": "postman"};
+            console.log(data);
+            axios.post('/api/history', data, {timeout: 5000})
+                .then((message) => {
+                    console.log(message);
+                })
+                .catch((error) => {
+                    console.log(error);
+                })
+            ;
+        },
+    },
+    watch: {
+        hover(){
+            console.log('hover called:', this.hover);
+            this.send();
+        }
+    }
+
 };
 
 </script>
